37ea388df1cf53a5d7bb44a73bb7be0ed516e4bd
ptPierre <pierrehohl2@gmail.com>
2024-05-09 12:25:40 +0200

    adding networks in network.ts

--- script/utils/networks.ts
+++ script/utils/networks.ts
@@ -11,10 +11,10 @@ export interface NetworkConfigs {
  * IMPORTANT: Increment all nextVersion after deployment please
  */
 export const networks: NetworkConfigs = {
-    "mainnet": {
-        studioEndpoint: "ethereum-blocks",
-        nextVersion: "v0.0.2"
-    },
+    // "mainnet": {
+    //     studioEndpoint: "ethereum-blocks",
+    //     nextVersion: "v0.0.4"
+    // },
     "arbitrum-one": {
         studioEndpoint: "arbitrum-one-blocks",
         nextVersion: "v0.0.1"
@@ -23,10 +23,10 @@ export const networks: NetworkConfigs = {
         studioEndpoint: "arbitrum-sepolia-blocks",
         nextVersion: "v0.0.1"
     },
-    "arweave-mainnet": {
-        studioEndpoint: "arweave-mainnet-blocks",
-        nextVersion: "v0.0.1"
-    },
+    // "arweave-mainnet": {
+    //     studioEndpoint: "arweave-mainnet-blocks",
+    //     nextVersion: "v0.0.1"
+    // },
     "aurora": {
         studioEndpoint: "aurora-blocks",
         nextVersion: "v0.0.1"
@@ -48,7 +48,7 @@ export const networks: NetworkConfigs = {
         nextVersion: "v0.0.1"
     },
     "bsc": {
-        studioEndpoint: "bsc-blocks",
+        studioEndpoint: "bnb-smart-chain-blocks",
         nextVersion: "v0.0.1"
     },
     "celo": {
@@ -63,10 +63,10 @@ export const networks: NetworkConfigs = {
         studioEndpoint: "chapel-blocks",
         nextVersion: "v0.0.1"
     },
-    "clover": {
-        studioEndpoint: "clover-blocks",
-        nextVersion: "v0.0.1"
-    },
+    // "clover": {
+    //     studioEndpoint: "clover-blocks",
+    //     nextVersion: "v0.0.1"
+    // },
     "fantom": {
         studioEndpoint: "fantom-blocks",
         nextVersion: "v0.0.1"
@@ -95,42 +95,42 @@ export const networks: NetworkConfigs = {
         studioEndpoint: "matic-blocks",
         nextVersion: "v0.0.1"
     },
-    "mbase": {
-        studioEndpoint: "mbase-blocks",
-        nextVersion: "v0.0.1"
-    },
-    "moonbeam": {
-        studioEndpoint: "moonbeam-blocks",
-        nextVersion: "v0.0.1"
-    },
-    "moonriver": {
-        studioEndpoint: "moonriver-blocks",
-        nextVersion: "v0.0.1"
-    },
-    "optimism": {
-        studioEndpoint: "optimism-blocks",
-        nextVersion: "v0.0.1"
-    },
-    "optimism-sepolia": {
-        studioEndpoint: "optimism-sepolia-blocks",
-        nextVersion: "v0.0.1"
-    },
-    "osmo-test-4": {
-        studioEndpoint: "osmo-test-4-blocks",
-        nextVersion: "v0.0.1"
-    },
-    "osmosis-1": {
-        studioEndpoint: "osmosis-1-blocks",
-        nextVersion: "v0.0.1"
-    },
-    "poa-core": {
-        studioEndpoint: "poa-core-blocks",
-        nextVersion: "v0.0.1"
-    },
-    "polygon-zkevm-testnet": {
-        studioEndpoint: "polygon-zkevm-testnet-blocks",
-        nextVersion: "v0.0.1"
-    },
+    // "mbase": {
+    //     studioEndpoint: "mbase-blocks",
+    //     nextVersion: "v0.0.1"
+    // },
+    // "moonbeam": {
+    //     studioEndpoint: "moonbeam-blocks",
+    //     nextVersion: "v0.0.1"
+    // },
+    // "moonriver": {
+    //     studioEndpoint: "moonriver-blocks",
+    //     nextVersion: "v0.0.1"
+    // },
+    // "optimism": {
+    //     studioEndpoint: "optimism-blocks",
+    //     nextVersion: "v0.0.1"
+    // },
+    // "optimism-sepolia": {
+    //     studioEndpoint: "optimism-sepolia-blocks",
+    //     nextVersion: "v0.0.1"
+    // },
+    // "osmo-test-4": {
+    //     studioEndpoint: "osmo-test-4-blocks",
+    //     nextVersion: "v0.0.1"
+    // },
+    // "osmosis-1": {
+    //     studioEndpoint: "osmosis-1-blocks",
+    //     nextVersion: "v0.0.1"
+    // },
+    // "poa-core": {
+    //     studioEndpoint: "poa-core-blocks",
+    //     nextVersion: "v0.0.1"
+    // },
+    // "polygon-zkevm-testnet": {
+    //     studioEndpoint: "polygon-zkevm-testnet-blocks",
+    //     nextVersion: "v0.0.1"
+    // },
     "polygon-zkevm": {
         studioEndpoint: "polygon-zkevm-blocks",
         nextVersion: "v0.0.1"
@@ -139,18 +139,18 @@ export const networks: NetworkConfigs = {
         studioEndpoint: "polygon-amoy-blocks",
         nextVersion: "v0.0.1"
     },
-    "sepolia": {
-        studioEndpoint: "sepolia-blocks",
-        nextVersion: "v0.0.1"
-    },
-    "zksync-era-sepolia": {
-        studioEndpoint: "zksync-era-sepolia-blocks",
-        nextVersion: "v0.0.1"
-    },
-    "zksync-era-testnet": {
-        studioEndpoint: "zksync-era-testnet-blocks",
-        nextVersion: "v0.0.1"
-    },
+    // "sepolia": {
+    //     studioEndpoint: "sepolia-blocks",
+    //     nextVersion: "v0.0.1"
+    // },
+    // "zksync-era-sepolia": {
+    //     studioEndpoint: "zksync-era-sepolia-blocks",
+    //     nextVersion: "v0.0.1"
+    // },
+    // "zksync-era-testnet": {
+    //     studioEndpoint: "zksync-era-testnet-blocks",
+    //     nextVersion: "v0.0.1"
+    // },
     "zksync-era": {
         studioEndpoint: "zksync-era-blocks",
         nextVersion: "v0.0.1"
